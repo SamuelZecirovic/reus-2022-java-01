8ed910de290c9ce43f5388c0300bb098adc4e5bc
Samuel Zecirovic <55589454+SamuelZecirovic@users.noreply.github.com>
2022-01-11 09:17:01 +0100

    mensaje para el repo

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+public class HolaMundo {    
+
+    public static void main(String[] args) {
+        System.out.println("whats your name?");
+        System.out.println("ezekiel");
+        System.out.println("fuck you, ezekiel");
+
+    }
+
+}
